8f58bbaa17ea08c18dd2c1374eed7aeb46477dd0
achafi <assia.chafii@hotmail.com>
2020-10-11 02:20:14 +0200

    add docker file and requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM continuumio/anaconda3:4.4.0
 COPY . /usr/app/
 EXPOSE 5000
 WORKDIR /usr/app/
-RUN pip install -r requirements.txt
-CMD python app.py
+RUN pip install --upgrade && pip install -r requirements.txt
+CMD python app1.py
--- app_streamlit.py
+++ app_streamlit.py
@@ -2,21 +2,22 @@ from flask import Flask, request, jsonify
 import numpy as np
 import pickle
 import pandas as pd
-from flasgger import Swagger
+#from flasgger import Swagger
+import streamlit as st 
 
-app=Flask(__name__)
-Swagger(app)
+#app=Flask(__name__)
+#Swagger(app)
 
 pickle_in = open("classifier.pkl","rb")
 classifier=pickle.load(pickle_in)
 
 
-@app.route('/')
+#@app.route('/')
 def welcome():
     return "Welcome All"
 
-@app.route('/predict',methods=["Get"])
-def predict_note_authentication():
+#@app.route('/predict',methods=["Get"])
+def predict_note_authentication(variance,skewness,curtosis,entropy):
     
     """Let's Authenticate the Banks Note 
     This is using docstrings for specifications.
@@ -43,17 +44,14 @@ def predict_note_authentication():
             description: The output values
         
     """
-
-    variance = request.args.get('variance')
-    skewness = request.args.get('skewness')
-    curtosis = request.args.get('curtosis')
-    entropy = request.args.get('entropy')
-    
-    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
-    return "The prediction value is " + str(prediction) 
+   
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    print(prediction)
+    return prediction
+ 
     
 
-@app.route('/predict_file',methods=["POST"])
+#@app.route('/predict_file',methods=["POST"])
 def predict_note_file():
     
     """Let's Authenticate the Banks Note 
@@ -75,7 +73,26 @@ def predict_note_file():
     prediction = classifier.predict(df_test)
     return str(list(prediction))
 
+def main():
+    st.title("Bank Authenticator")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    variance = st.text_input("Variance","Type Here")
+    skewness = st.text_input("skewness","Type Here")
+    curtosis = st.text_input("curtosis","Type Here")
+    entropy = st.text_input("entropy","Type Here")
+    result=""
+    if st.button("Predict"):
+        result=predict_note_authentication(variance,skewness,curtosis,entropy)
+    st.success('The output is {}'.format(result))
+    if st.button("About"):
+        st.text("Lets LEarn")
+        st.text("Built with Streamlit")
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=8000)
-    
+    #app.run(host='0.0.0.0',port=8000)
+    main()
